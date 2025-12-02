a18ee1a56484c0662bbee87cc1f25dfdb069d94d
Thibault Cheneviere <thibault.chene23@gmail.com>
2025-12-02 14:25:56 +0100

    feat: added readme year

---
+++ src/bin/01.rs
@@ -0,0 +1,28 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(not(feature = "test_lib"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    advent_of_code::part!();
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_example(DAY, PART_ONE));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_example(DAY, PART_TWO));
+        assert_eq!(result, None);
+    }
+}
